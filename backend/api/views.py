207c1e1b40ba8fbed634b0ca15f8db8274fdceb5
Vlad Brusensky <brusen_sky@mail.ru>
2022-10-23 16:16:03 +0300

    Optimize imports.

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users.models import Follow, User
-
 from .pagination import LimitPagination
 from .serializers import SubscriptionsSerializer
 
--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from rest_framework.generics import get_object_or_404
 
 from users.serializers import SerializerCustomUser
-
 from .models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
                      ShoppingCart, Tag)
 
